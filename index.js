4cca877e1fc6a8a3e856a3014b3aceb764fd99da
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-04-11 21:35:32 +0700

    C1-S2-PRACTICE

---
+++ index.js
@@ -0,0 +1,10 @@
+let numbers = [1,2,3,4,5];
+numbers.push(6);
+numbers.forEach((element)=> console.log(element));
+
+let index = 2;
+numbers.at(index);
+
+numbers.slice(1);
+numbers.reverse();
+numbers.filter((number)=>number>5 );
